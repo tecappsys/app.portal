b6d90de5e345baebdacfc7ba9da44c5c94175f98
Daniel <danieldiazmc@gmail.com>
2023-12-28 04:05:28 -0300

    v16

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -17,37 +17,37 @@
 # NODE JS
 
 # Define the expected Node.js version
-expected_version="16.20.2"
-install_node(){
-    echo ..............................installin Node whit NVM...
-    export NVM_DIR=$HOME/.nvm;
-    source $NVM_DIR/nvm.sh;
-    source ~/.bashrc
-    nvm install node -y
-    nvm install 16.20.2
-    nvm use 16.20.2
-    echo ..............................Version nodejs...
-    node -v
-    echo ..............................Version npm...
-    npm -v  
-}
+# expected_version="16.20.2"
+# install_node(){
+#     echo ..............................installin Node whit NVM...
+#     export NVM_DIR=$HOME/.nvm;
+#     source $NVM_DIR/nvm.sh;
+#     source ~/.bashrc
+#     nvm install node -y
+#     nvm install 16.20.2
+#     nvm use 16.20.2
+#     echo ..............................Version nodejs...
+#     node -v
+#     echo ..............................Version npm...
+#     npm -v  
+# }
 
 # Check if Node.js is not installed or version differs
-if ! command -v node &> /dev/null; then
-    install_node
-else
-    # Get the installed Node.js version
-    installed_version=$(node -v | cut -c 2-)  # Remove the 'v' from the version
+# if ! command -v node &> /dev/null; then
+#     install_node
+# else
+#     # Get the installed Node.js version
+#     installed_version=$(node -v | cut -c 2-)  # Remove the 'v' from the version
 
-    # Compare the installed version with the expected version
-    if [ "$installed_version" != "$expected_version" ]; then
-        install_node
-    fi
-fi
+#     # Compare the installed version with the expected version
+#     if [ "$installed_version" != "$expected_version" ]; then
+#         install_node
+#     fi
+# fi
 
 # ANGULAR CLI
-if ! command -v ng &> /dev/null; then
-    echo ..............................install Angular/Cli...
-    sudo npm i -g @angular/cli
-fi
+# if ! command -v ng &> /dev/null; then
+#     echo ..............................install Angular/Cli...
+#     sudo npm i -g @angular/cli
+# fi
 
