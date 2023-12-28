e858ffb1493786c131c2837971d5f27b6e6d2c80
Daniel <danieldiazmc@gmail.com>
2023-12-28 09:44:26 -0300

    v16

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -30,3 +30,17 @@
 # ANGULAR CLI
 # echo ..............................install Angular/Cli...
 # sudo npm i @angular/cli@16.2.11
+
+
+export HOME="/home/ubuntu/"
+export NVM_DIR="$HOME/.nvm/"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+source ~/.bashrc
+
+echo ..............................Change directory...
+cd /var/www/app/portal
+
+echo ..............................Install dependencies Package...
+npm install --loglevel verbose
