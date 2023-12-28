a3b63f4cc65a1eb46f21fe22b17620640684047f
Daniel <danieldiazmc@gmail.com>
2023-12-28 04:33:04 -0300

    update before.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,5 @@
-export NVM_DI="/home/ubuntu/.nvm"
-echo $NVM_DIR
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "/home/ubuntu/.nvm/nvm.sh" ] && \. "/home/ubuntu/.nvm/nvm.sh"  # This loads nvm
+[ -s "/home/ubuntu/.nvm/bash_completion" ] && \. "/home/ubuntu/.nvm/bash_completion"  # This loads nvm bash_completion
 
 
 echo ..............................Change directory...
