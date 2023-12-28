73cce207d97b9d65e058172c7a03f86789f22f0b
Daniel <danieldiazmc@gmail.com>
2023-12-28 09:45:40 -0300

    v16

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,5 +1,5 @@
 export HOME="/home/ubuntu/"
-export NVM_DIR="$HOME/.nvm/"
+export NVM_DIR="$HOME/.nvm/versions/node/v16.20.2/bin"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
@@ -7,6 +7,9 @@ source ~/.bashrc
 
 echo ..............................Change directory...
 cd /var/www/app/portal
+
+echo ..............................Install dependencies Package...
+npm install --loglevel verbose
 
 echo ..............................Build project...
 npm run build --prod
