ad1b29723680be85d96ad8779f6e57ccfa4490f8
Daniel <danieldiazmc@gmail.com>
2023-12-28 04:31:04 -0300

    update before.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,8 +1,8 @@
-export NVM_DIR="$HOME/.nvm"
+export NVM_DI="/home/ubuntu/.nvm"
 echo $NVM_DIR
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-export HOME="/home/ubuntu/"
+
 
 echo ..............................Change directory...
 cd /var/www/app/portal
