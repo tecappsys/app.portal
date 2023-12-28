b4c4b0c1b09c49c1cdf171a6643e90f0f7202e6a
Daniel <danieldiazmc@gmail.com>
2023-12-28 04:44:39 -0300

    update before.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,8 +1,15 @@
+export HOME="/home/ubuntu/"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+source ~/.bashrc
+
 echo ..............................Change directory...
 cd /var/www/app/portal
 
 echo ..............................Install dependencies Package...
-sudo npm install --loglevel verbose
+npm install --loglevel verbose
 
 echo ..............................Build project...
-sudo npm run build --prod
+npm run build --prod
