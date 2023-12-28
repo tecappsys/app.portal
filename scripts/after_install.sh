e26bca2571bce1c723e89260986cd0f3a2ec1377
Daniel <danieldiazmc@gmail.com>
2023-12-28 08:42:56 -0300

    update before.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,6 +1,6 @@
 export HOME="/home/ubuntu/"
 export NVM_DIR="$HOME/.nvm/versions/node/v16.20.2/bin"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/npm.sh" ] && \. "$NVM_DIR/npm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 source ~/.bashrc
