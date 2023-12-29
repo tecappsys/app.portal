860f99965d221120ceda183eb044030035293f8b
Daniel <danieldiazmc@gmail.com>
2023-12-29 06:39:32 -0300

    upload dist

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,15 +1,15 @@
-export HOME="/home/ubuntu/"
-export NVM_DIR="$HOME/.nvm/"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# export HOME="/home/ubuntu/"
+# export NVM_DIR="$HOME/.nvm/"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-source ~/.bashrc
+# source ~/.bashrc
 
-echo ..............................Change directory...
-cd /var/www/app/portal
+# echo ..............................Change directory...
+# cd /var/www/app/portal
 
-echo ..............................Install dependencies Package...
-npm install --loglevel verbose
+# echo ..............................Install dependencies Package...
+# npm install --loglevel verbose
 
-echo ..............................Build project...
-npm run build --prod
+# echo ..............................Build project...
+# npm run build --prod
