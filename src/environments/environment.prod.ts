a9b5c1065283b1e89ee782b0e6ed5393764b54f7
Daniel <danieldiazmc@gmail.com>
2025-03-01 03:15:22 -0400

    ajustes enviroment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://tecappsys.com/portfolio/api/node',
+    app_home: 'https://tecappsys.com',
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,5 @@
 export const environment = {
-    app_home:'http://tecappsys.com',
+    production: false,
+    apiUrl: 'http://localhost:3000/api',
+    app_home: 'http://localhost:4200',
 };
