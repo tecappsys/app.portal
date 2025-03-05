30ca6c1ec4cd1a5337133db734064601ec1e1c3b
Daniel <danieldiazmc@gmail.com>
2025-03-05 02:14:25 -0400

    update logotipos

--- src/app/shared/icons/svg-logo/svg-logo.component.ts
+++ src/app/shared/icons/svg-logo/svg-logo.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'svg-logo',
   templateUrl: './svg-logo.component.html',
-  styleUrls: ['./svg-logo.component.scss']
 })
 export class SVGLogoComponent {
 
